d37bc0ca6d1d8365799409b68f5a4a87b55b5581
unknown <zuu0319@ajou.ac.kr>
2021-11-17 16:55:08 +0900

    웹캠 연결

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### npm install
+### npm install socket.io-client
 ### npm start
 
 ./src/cv에서 파이썬 프로젝트 생성, model은 .gitignore에 추가
--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import './style/App.css';
+import React, { useState, useEffect } from 'react';
 import Navigation from './Navigation';
 import RecycleLabel from './RecycleLabel';
+import Cam from './cv/templates';
+import io from "socket.io-client";
+
+let endPoint = "http://localhost:5000";
+let socket = io.connect(`${endPoint}`)
 
 function App() {
+  const [labelColor, setLabelColor] = useState(['gray', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray']);
+
+  useEffect(() => {
+    getMessages();
+  }, labelColor);
+
+  const getMessages = () => {
+    socket.on("message", msg => {
+      const newLabelColor = ['gray', 'gray', 'gray', 'gray', 'gray', 'gray', 'gray'];
+      console.log(msg);
+      setLabelColor(newLabelColor);
+    });
+  }
+
   return (
     <div className="App">
       <Navigation></Navigation>
       <div className="container">
         <div className="webcam-box">
-
+          <Cam/>
         </div>
         <div className="contents">
           <div className="label-box">
